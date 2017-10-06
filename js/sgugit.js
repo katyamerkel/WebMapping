74d6626c43fa7a78871bc26a399cdbe36a0f9d4d
katyamerkel <katya_merkel@mail.ru>
2017-10-06 17:56:17 +0700

    october06

---
+++ js/sgugit.js
@@ -0,0 +1,15 @@
+﻿var mapdata = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "id": 1, 
+"labeltext": "СГУГиТ", "imageurl": 
+"http:\/\/www.informio.ru\/imgs\/fotos\/sgugit1.jpg" },
+"geometry": { "type": "Polygon", 
+"coordinates": [ [ [ 82.862532467887618, 54.98712823119206 ], 
+[ 82.863009850002513, 54.987150743759138 ], 
+[ 82.863179876509193, 54.986002586739026 ], 
+[ 82.862532467887618, 54.98712823119206 ] ] ] } }
+]
+}
